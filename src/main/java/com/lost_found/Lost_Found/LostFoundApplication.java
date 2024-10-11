ae0dfcc90d3c1783e6bd41c919914111169d1718
sharon <opudosharon6@gmail.com>
2024-10-11 09:45:47 +0300

    added bootstrap 5.3.3

---
+++ src/main/java/com/lost_found/Lost_Found/LostFoundApplication.java
@@ -0,0 +1,13 @@
+package com.lost_found.Lost_Found;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostFoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostFoundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lost_found/Lost_Found/LostFoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lost_found.Lost_Found;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostFoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
